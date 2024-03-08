0b560e8bdc80910fbcdbfb83485f7b479ea0b562
suwon <ysw7939@gmail.com>
2024-03-08 11:42:26 +0900

    24.03.08 백준 한접시

---
+++ big_coding_test/Oneday/2231.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int arr[1100004];
+    fill(arr, arr + 1000000, 0);
+    int idx, N;
+    int i = 1;
+    cin >> N;
+    while (true)
+    {
+        int val;
+
+        if (i < 10)
+        {
+            val = i + i;
+        }
+        else if (i < 100)
+        {
+            val = i + (i / 10) + (i % 10);
+        }
+        else if (i < 1000)
+        {
+            val = i + (i / 100) + (i % 100 / 10) + (i % 10);
+        }
+        else if (i < 10000)
+        {
+            val = i + (i / 1000) + (i % 1000 / 100) + (i % 100 / 10) + (i % 10);
+        }
+        else if (i < 100000)
+        {
+            val = i + (i / 10000) + (i % 10000 / 1000) + (i % 1000 / 100) + (i % 100 / 10) + (i % 10);
+        }
+        else if (i < 1000000)
+        {
+            val = i + (i / 100000) + (i % 100000 / 10000) + (i % 10000 / 1000) + (i % 1000 / 100) + (i % 100 / 10) + (i % 10);
+        }
+        else
+        {
+            break;
+        }
+
+        if (arr[val] == 0)
+        {
+            arr[val] = i;
+        }
+        i++;
+    }
+
+    // for (int j = 0; j < 200; j++)
+    // {
+    //     cout << j << ":: " << arr[j] << "\n";
+    // }
+    cout << arr[N] << "\n";
+}
