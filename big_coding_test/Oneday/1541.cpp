d0475909b7fc2c3db4a25a6e9b47563096b9ae56
suwon <ysw7939@gmail.com>
2024-03-19 18:24:49 +0900

    2024.03.19 백준

--- big_coding_test/Oneday/1541.cpp
+++ big_coding_test/Oneday/1541.cpp
@@ -1,8 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a, b;
-    cin >> a >> b;
-    cout << a - b << '\n';
+vector<string> split(const string &input, string delimiter)
+{
+    vector<string> result;
+    auto start = 0;
+    auto end = input.find(delimiter);
+    while (end != string::npos)
+    {
+        result.push_back(input.substr(start, end - start));
+        start = end + delimiter.size();
+        end = input.find(delimiter, start);
+    }
+    result.push_back(input.substr(start));
+    return result;
+}
+
+int add(string s)
+{
+    vector<string> sumArr = split(s, "+");
+    int result = 0;
+    for (string str : sumArr)
+    {
+        result += stoi(str);
+    }
+    return result;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+    vector<string> sarr = split(s, "-");
+    vector<int> intArr;
+    for (string s : sarr)
+    {
+        intArr.push_back(add(s));
+    }
+    int result = intArr[0];
+    for (int i = 1; i < intArr.size(); i++){
+        result -= intArr[i];
+    }
+
+    cout << result << '\n';
 }
