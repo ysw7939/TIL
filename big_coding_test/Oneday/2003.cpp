166fec5027b6259d0d4ec04a066842163596b18a
suwon <ysw7939@gmail.com>
2024-03-24 12:27:31 +0900

    24.03.24 백준

--- big_coding_test/Oneday/2003.cpp
+++ big_coding_test/Oneday/2003.cpp
@@ -7,37 +7,42 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, S;
-    cin >> N >> S;
+    int N, M;
+    cin >> N >> M;
     int arr[N + 4];
     for (int i = 1; i <= N; i++)
     {
         cin >> arr[i];
     }
 
-    int end = 1;
     int start = 1;
-    int result = INT_MAX;
+    int end = 1;
+    int cnt = 0;
     int sum = arr[1];
+
     while (start <= end && end <= N)
     {
-        if (sum >= S)
-            result = min(result, end - start + 1);
-        if (sum < S)
+        if (sum == M)
+            cnt++;
+        if (sum <= M)
         {
             end++;
             sum += arr[end];
         }
         else
         {
-            sum -= arr[start];
-            start++;
+            if (start == end)
+            {
+                end++;
+                sum += arr[end];
+            }
+            else
+            {
+                sum -= arr[start];
+                start++;
+            }
         }
     }
-    if(result  == INT_MAX) {
-        cout << 0 << '\n';
-        return 0;
-    }
 
-    cout << result << '\n';
+    cout << cnt << '\n';
 }
