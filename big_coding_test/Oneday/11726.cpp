33bb1712af38c6fb9ac1edeb76214c08a18f331c
suwon <ysw7939@gmail.com>
2024-03-27 20:58:34 +0900

    24.03.27 백준

---
+++ big_coding_test/Oneday/11726.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long memo[1004] = {0, 1, 2, 3};
+long long moduler = 10007;
+long long dp(int i)
+{
+    if (memo[i] != 0)
+    {
+        return memo[i];
+    }
+    return memo[i] = (dp(i - 2) + dp(i - 1)) % moduler;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    long long result = dp(N);
+
+    cout << result << '\n';
+}
